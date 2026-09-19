5b13b239a7251441ea6e9f6b6b6b35078210b316
CaspervaH <64915149+CaspervaH@users.noreply.github.com>
2026-09-19 20:59:27 +0200

    Tests uitgebreid: validatie + live-check voor border.html/border.js

--- tests/validate.js
+++ tests/validate.js
@@ -1,4 +1,4 @@
-// Valideert index.html + map.js van de Tuinplantplanner
+// Valideert index.html + map.js + border.html + border.js van de Tuinplantplanner
 // Draait in GitHub Actions bij elke push (node 20).
 const fs = require('fs');
 
@@ -9,16 +9,20 @@ const err = (msg) => { errors.push(msg); console.error('  \u2717 ' + msg); };
 function main() {
   console.log('== Tuinplantplanner validatie ==\n');
 
-  for (const f of ['index.html', 'map.js']) {
+  for (const f of ['index.html', 'map.js', 'border.html', 'border.js']) {
     if (!fs.existsSync(f)) { err(f + ' ontbreekt'); return fail(); }
     ok(f + ' bestaat (' + fs.statSync(f).size + ' bytes)');
   }
   const html = fs.readFileSync('index.html', 'utf8');
   const mapJs = fs.readFileSync('map.js', 'utf8');
+  const borderHtml = fs.readFileSync('border.html', 'utf8');
+  const borderJs = fs.readFileSync('border.js', 'utf8');
 
   // HTML structuur
-  if (!html.trimEnd().endsWith('</html>')) err('index.html eindigt niet op </html> (mogelijk afgekapt!)');
-  else ok('index.html is compleet');
+  for (const [naam, file] of [['index.html', html], ['border.html', borderHtml]]) {
+    if (!file.trimEnd().endsWith('</html>')) err(naam + ' eindigt niet op </html> (mogelijk afgekapt!)');
+    else ok(naam + ' is compleet');
+  }
   if (!html.includes('https://unpkg.com/leaflet@1.9.4/dist/leaflet.js')) err('leaflet.js library ontbreekt (kaart blijft grijs!)');
   else ok('leaflet.js library geladen');
   if (!html.includes('https://unpkg.com/leaflet@1.9.4/dist/leaflet.css')) err('leaflet.css ontbreekt');
@@ -27,24 +31,37 @@ function main() {
   else ok('map.js wordt geladen');
   if (/leaflet@1.9.4\/dist\/leaflet.js" defer><\/script>\s*<script src="map.js" defer>/.test(html)) ok('leaflet.js laadt v\u00f3\u00f3r map.js');
   else err('map.js moet NA leaflet.js geladen worden');
+  if (!/<script src="border.js" defer>/.test(borderHtml)) err('border.js wordt niet geladen door border.html');
+  else ok('border.js wordt geladen door border.html');
 
   // JS-syntax
   const scripts = [...html.matchAll(/<script>([\s\S]*?)<\/script>/g)].map(m => m[1]);
   scripts.forEach((s, i) => {
-    try { new Function(s); ok('inline script #' + (i + 1) + ' parseert'); }
-    catch (e) { err('inline script #' + (i + 1) + ' SYNTAXFOUT: ' + e.message); }
+    try { new Function(s); ok('index inline script #' + (i + 1) + ' parseert'); }
+    catch (e) { err('index inline script #' + (i + 1) + ' SYNTAXFOUT: ' + e.message); }
   });
-  try { new Function(mapJs); ok('map.js parseert'); }
-  catch (e) { err('map.js SYNTAXFOUT: ' + e.message); }
-
-  // getElementById-doelen bestaan in de HTML
-  const allJs = mapJs + scripts.join('\n');
-  const ids = new Set([...allJs.matchAll(/getElementById\('([^']+)'\)/g)].map(m => m[1]));
-  let missing = 0;
-  for (const id of ids) {
-    if (!html.includes('id="' + id + '"')) { err("getElementById('" + id + "') maar geen element met dat id in HTML"); missing++; }
+  const borderScripts = [...borderHtml.matchAll(/<script>([\s\S]*?)<\/script>/g)].map(m => m[1]);
+  borderScripts.forEach((s, i) => {
+    try { new Function(s); ok('border inline script #' + (i + 1) + ' parseert'); }
+    catch (e) { err('border inline script #' + (i + 1) + ' SYNTAXFOUT: ' + e.message); }
+  });
+  for (const [naam, src] of [['map.js', mapJs], ['border.js', borderJs]]) {
+    try { new Function(src); ok(naam + ' parseert'); }
+    catch (e) { err(naam + ' SYNTAXFOUT: ' + e.message); }
   }
-  if (!missing && ids.size) ok('alle ' + ids.size + ' aangesproken element-ids bestaan in de HTML');
+
+  // getElementById-doelen bestaan in de juiste HTML
+  function checkIds(jsSources, htmlText, htmlNaam) {
+    const allJs = jsSources.join('\n');
+    const ids = new Set([...allJs.matchAll(/getElementById\('([^']+)'\)/g)].map(m => m[1]));
+    let missing = 0;
+    for (const id of ids) {
+      if (!htmlText.includes('id="' + id + '"')) { err(htmlNaam + ": getElementById('" + id + "') maar geen element met dat id in " + htmlNaam); missing++; }
+    }
+    if (!missing && ids.size) ok('alle ' + ids.size + ' aangesproken element-ids bestaan in ' + htmlNaam);
+  }
+  checkIds([mapJs, ...scripts], html, 'index.html');
+  checkIds([borderJs, ...borderScripts], borderHtml, 'border.html');
 
   // HTML-specifieke onderdelen
   const htmlMust = {
@@ -54,8 +71,17 @@ function main() {
     'plantenlijst': 'id="plantList"'
   };
   for (const [naam, needle] of Object.entries(htmlMust)) {
-    if (html.includes(needle)) ok('bevat: ' + naam);
-    else err('mist: ' + naam);
+    if (html.includes(needle)) ok('index bevat: ' + naam);
+    else err('index mist: ' + naam);
+  }
+  const borderMust = {
+    'svg-view': 'id="shapeSvg"',
+    'plantenlijst-palette': 'id="paletteList"',
+    'tabel': 'id="plantTableBody"'
+  };
+  for (const [naam, needle] of Object.entries(borderMust)) {
+    if (borderHtml.includes(needle)) ok('border.html bevat: ' + naam);
+    else err('border.html mist: ' + naam);
   }
 
   // map.js-specifieke onderdelen
@@ -73,8 +99,10 @@ function main() {
   }
 
   // Geen inline onclick (breukgevoelig)
-  if (/<[^>]+\sonclick=/.test(html)) err('inline onclick gevonden (breukgevoelig met apostroffen in plantnamen)');
-  else ok('geen inline onclick-handlers');
+  for (const [naam, file] of [['index.html', html], ['border.html', borderHtml]]) {
+    if (/<[^>]+\sonclick=/.test(file)) err(naam + ': inline onclick gevonden (breukgevoelig met apostroffen in plantnamen)');
+    else ok(naam + ': geen inline onclick-handlers');
+  }
 
   fail();
 }
